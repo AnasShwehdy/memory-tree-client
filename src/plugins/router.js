0a8c8ef61d1ba21fcb496519ca6daef70fdfa885
Anas Shwehdy <52764992+AnasShwehdy@users.noreply.github.com>
2024-07-22 11:39:02 +0200

    Update router.js

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -6,10 +6,7 @@ const routes = [
     },
     {
         path: '/history', name: 'History',  component: () => import('@/views/History.vue')
-    },
-    {
-        path: '/code', name: 'Code',  component: () => import('@/views/Code.vue')
-    },
+    }
 ]
 
 const router = createRouter({
