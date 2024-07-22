fc5122627027ea4b8d6cebaf1278874486f5fc86
Anas Shwehdy <52764992+AnasShwehdy@users.noreply.github.com>
2024-07-22 11:38:39 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <script setup>
-import { RouterView, RouterLink } from 'vue-router'
+import { RouterView } from 'vue-router'
 
 </script>
 
 <template>
   <div class="app">
-    <div style="display: flex;
-    align-items: baseline;
-    justify-content: space-evenly;
-">
-      <router-link to="/">Home</router-link>
-      <router-link to="/code">Encoding</router-link>
-    </div>
-
     <div style="margin:80px;">
       <router-view></router-view>
     </div>
